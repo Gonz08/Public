240741bad373fbe24e42111d9c0480d29206ebcd
Gonz08 <59067327+Gonz08@users.noreply.github.com>
2020-02-23 00:15:40 +0100

    Update 001-basic-settings.sh

--- Ubuntu/001-basic-settings.sh
+++ Ubuntu/001-basic-settings.sh
@@ -6,9 +6,9 @@ echo "############################"
 echo "############################"
 echo "############################"
 
-sudo add-apt-repository ppa:bit-team/stable -y
-
-
+#sudo add-apt-repository ppa:bit-team/stable -y
+sudo bash -c 'echo "deb https://labs.picotech.com/debian/ picoscope main" >/etc/apt/sources.list.d/picoscope.list'
+wget -O - https://labs.picotech.com/debian/dists/picoscope/Release.gpg.key | sudo apt-key add -
 echo "############################"
 
 #Update
@@ -16,15 +16,25 @@ sudo apt-get update -y
 echo "############################"
 #upgrade
 sudo apt-get upgrade -y
-echo "############################"
 sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
 echo "############################"
+echo "############## User werden erstellt##############"
+echo -n "ENTER Taste um fortzufahren ..." 
+read input
+sudo adduser  birgit && adduser birgit sudo
+read user
+sudo adduser  kinder 
+
 #Programme installieren
 echo "############################"
 sudo snap install spotify 
-echo "############################"
+sudo apt-get install picoscope
+sudo apt-get install libreoffice libreoffice-l10n-de libreoffice-help-de -y
 sudo apt-get install gparted -y
-echo "############################"
 sudo apt-get install backintime-qt4 
-echo "############################"
 sudo apt-get install freeplane -y
+sudo apt-get install freecad freecad-doc
+sudo apt-get install chromium
+
+echo "############################"
