e107bef6686e994001da72edf72625a65c9e2497
Gonz08 <59067327+Gonz08@users.noreply.github.com>
2020-02-23 13:31:11 +0100

    Update 001-basic-settings.sh

--- Ubuntu/001-basic-settings.sh
+++ Ubuntu/001-basic-settings.sh
@@ -4,8 +4,7 @@
 
 echo "############################"
 echo "############################"
-echo "############################"
-
+echo "##############   Weitere Respositoris werden eingebunden ##############"
 #sudo add-apt-repository ppa:bit-team/stable -y
 sudo bash -c 'echo "deb https://labs.picotech.com/debian/ picoscope main" >/etc/apt/sources.list.d/picoscope.list'
 wget -O - https://labs.picotech.com/debian/dists/picoscope/Release.gpg.key | sudo apt-key add -
@@ -14,20 +13,33 @@ echo "############################"
 #Update
 sudo apt-get update -y
 echo "############################"
-#upgrade
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get autoremove -y
-echo "############################"
 echo "############## User werden erstellt##############"
 echo -n "ENTER Taste um fortzufahren ..." 
 read input
 sudo adduser  birgit && adduser birgit sudo
+sudo adduser birgit tj
+sudo chmod u=rw,g=rw,o=r /home/all
 read user
 sudo adduser  kinder 
 
-#Programme installieren
-echo "############################"
+
+echo "########### Updates werden installiert #################"
+echo -n "ENTER Taste um fortzufahren ..." 
+read input
+
+#upgrade
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+
+echo -n "ENTER Taste um fortzufahren ..." 
+read input
+
+echo "############### weitere Programme installieren##############"
+
+echo -n "ENTER Taste um fortzufahren ..." 
+read input
+echo "############## >>>>##############"
 sudo snap install spotify 
 sudo apt-get install picoscope -y
 sudo apt-get install libreoffice libreoffice-l10n-de libreoffice-help-de -y
