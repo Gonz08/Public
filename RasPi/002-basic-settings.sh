dba298e4bd1d30510622417278b530e8b2b1ab8b
Gonz08 <59067327+Gonz08@users.noreply.github.com>
2020-02-26 14:26:47 +0100

    Update 002-basic-settings.sh

--- RasPi/002-basic-settings.sh
+++ RasPi/002-basic-settings.sh
@@ -4,26 +4,11 @@ echo "############# Löschen des Users 'pi' ###############"
 sudo killall -u pi
 sudo deluser -remove-home pi
 
-echo
+echo "############# Hier könnnten noch Einträge für fstab folgen###############"
 
-
-echo "############# Docker installieren ###############"
 curl wttr.in/Traunstein
-sudo curl -fsSL https://get.docker.com | sh
 
-sudo usermod -aG docker tj
-
-
-echo "############# Docker Check ###############"
-sudo docker run hello-world
-
-echo "############# Docker Compose ###############"
-sudo apt-get install libffi-dev libssl-dev
-sudo apt-get install -y python python-pip
-sudo apt-get remove python-configparser
-sudo pip install docker-compose 
-
-
+pip install docker-compose 
 
 echo "############# jetzt kann wieder in die GUI gebootet werden ###############"
 
