8602b1a4e54110bb3a53f1b57bf5438f7e107f5b
Gonz08 <59067327+Gonz08@users.noreply.github.com>
2021-03-22 22:58:13 +0100

    Update start_smarthome.sh

--- MiniPC/start_smarthome.sh
+++ MiniPC/start_smarthome.sh
@@ -1,5 +1,33 @@
 
 
+#!/bin/bash
+
+#Pakete ergänzen
+
+echo "############################"
+echo "############################"
+echo "##############   Weitere Respositoris werden eingebunden ##############"
+
+sudo apt-get update -y
+sudo apt-get install curl -y
+sudo apt-get install wget -y
+
+#wget -c https://syncthing.net/release-key.txt | sudo apt-key add -
+#echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
 
-sudo apt install mosquitto -y
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+echo "############### Fish als Bash nstallieren##############"
+sudo apt-get install fish -y
+chsh -s /usr/bin/fish 
+echo "############### installing software ##############"
+
+sudo apt-get install mosquitto -y
+sudo apt-get install mosquitto-clients -y
+
+sudo apt-get install build-essential git -y
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+sudo systemctl enable nodered.service
+sudo systemctl start nodered.service
