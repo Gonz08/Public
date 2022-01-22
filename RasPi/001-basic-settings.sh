402a100d7c364505c39c9377550ea1f615948a49
Gonz08 <59067327+Gonz08@users.noreply.github.com>
2022-01-22 17:11:06 +0100

    Update 001-basic-settings.sh

--- RasPi/001-basic-settings.sh
+++ RasPi/001-basic-settings.sh
@@ -3,8 +3,8 @@
 echo
 echo "############# System Updates/Upgrades ausführen & Installation der 'unattended_upgrades' ###############"
 sudo apt update -y  
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 
 echo "############# Zusätzliche Software' ###############"
 sudo apt-get install fish -y
@@ -12,44 +12,34 @@ chsh -s /usr/bin/fish
 
 echo "############# Zusätzliche Software' ###############"
 
-sudo apt-get install gparted -y
-ec
-
-
-sudo apt-get install synaptic -y
+#sudo apt install gparted -y
+#sudo apt-get install synaptic -y
 #sudo apt-get install libreoffice -y
-sudo apt-get install gnome-disk-utility -y
-sudo apt-get install nautilus -y
-sudo apt-get install lshw -y
-sudo apt-get install curl -y
-sudo apt-get install wget -y
-sudo apt install xscreensaver -y
-
+#sudo apt-get install gnome-disk-utility -y
+#sudo apt-get install nautilus -y
+#sudo apt-get install lshw -y
+#sudo apt-get install curl -y
+#sudo apt-get install wget -y
+#sudo apt install xscreensaver -y
 sudo apt autoremove -y
 
-echo "############# Anlage eines neuen Users und Passwort vergeben ' ###############"
-user="tj"
-echo $user
- 
-sudo useradd -m $user #User mit Homeverzeichnis anlegen
-sudo gpasswd -a $user sudo
-sudo gpasswd $user 
+echo "############# syncthing installieren ###############"
+sudo apt-get install curl -y
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update
+sudo apt install syncthing -y
 
-echo "############# Aufnahme in alle Gruppen in denen Pi Mitglied ist ' ###############"
-sudo adduser $user adm
-sudo adduser $user dialout
-sudo adduser $user cdrom
-sudo adduser $user sudo
-sudo adduser $user audio
-sudo adduser $user video
-sudo adduser $user plugdev
-sudo adduser $user games
-sudo adduser $user users
-sudo adduser $user input
-sudo adduser $user netdev
-sudo adduser $user spi
-sudo adduser $user i2c
-sudo adduser $user gpio
+
+
+sudo systemctl enable syncthing@pi.service
+sudo systemctl start syncthing@pi.service
+
+
+
+
+
+
 
 echo "############# Installation abgeschlossen, das System wird neu gestartet ###############"
 sudo reboot 
